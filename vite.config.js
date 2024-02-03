407347fba4b1a27489e6fd304e156b936f95d3bd
Trrteam24a <157940469+Trrteam24a@users.noreply.github.com>
2024-02-03 15:04:59 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base:"/Frontend-vue-app",
+  base:"/Frontend-vue-Backend-app",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
