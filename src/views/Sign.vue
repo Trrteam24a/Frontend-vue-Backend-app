7838f8bea4d4a491aa53f71ec364abe6b27fe20e
Trrteam24a <157940469+Trrteam24a@users.noreply.github.com>
2024-02-03 12:23:43 +0530

    Add files via upload

---
+++ src/views/Sign.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container">
+    <h1>Open Your Account</h1>
+    <p>
+      <button @click="signInWithGoogle" class="google-button">
+        <img
+          src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
+          alt="Google Logo"
+          class="google-logo"
+        />
+        Sign In With Google
+      </button>
+    </p>
+    <p v-if="registeredUsername">Registered username: {{ registeredUsername }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const registeredUsername = ref("");
+
+const signInWithGoogle = async () => {
+  try {
+    const result = await signInWithPopup(getAuth(), new GoogleAuthProvider());
+    const user = result.user;
+    router.push("/feed");
+    registeredUsername.value = extractUsername(user.email);
+  } catch (error) {
+  }
+};
+
+const extractUsername = (email) => {
+  return email.split('@')[0];
+};
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+}
+
+.google-button {
+  background-color: white;
+  border: 1px solid #4285F4;
+  color: #4285F4;
+  padding: 10px 15px;
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  box-shadow: 1px 4px 10px rgba(25, 24, 24, 0.1);
+}
+
+.google-logo {
+  width: 20px;
+  margin-right: 10px;
+}
+</style>
